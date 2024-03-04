207e383e2b40f26b43bb8f3eea71b6c921528c30
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2024-03-04 08:12:12 -0600

    Update from lenovo

---
+++ org/scanf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main() {
+// declare variables
+int i;
+
+// ask for input
+puts("Enter an integer: ");
+
+// scan integer value i
+scanf("%d", &i);
+
+// print scanned value
+printf("Your input was: %d\n", i);
+
+return 0;
+}
