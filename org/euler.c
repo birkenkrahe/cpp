a2d3fbed3592095dd87adc5bc35360a97a16041c
Marcus Birkenkrahe <377079+birkenkrahe@users.noreply.github.com>
2024-04-23 22:47:20 -0500

    Update from vostro

---
+++ org/euler.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+
+
+
+#include <stdio.h> // input / output
+#include <math.h> // math constants and functions
+
+int main(void)  // function 1
+{
+  const double E = 2.7182818; 
+
+  printf("%g\n", log(E));  // function 2 + 3
+  return 0;
+}
---
+++ org/hello_world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main() {
+// reusable function definition
+void hello_world(void)
+{
+  printf("Hello world\n"); // what the function does
+}
+// reusable function call
+hello_world(); 
+hello_world();
+hello_world();
+return 0;
+}
